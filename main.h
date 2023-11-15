1543782f230e86dbe2c086fa1dab13f849ca5d8e
Mohakad <mohkadtrd@gmail.com>
2023-11-15 21:29:45 +0300

    corr

--- main.h
+++ main.h
@@ -8,6 +8,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define COMM_LENGTH (1024)
+#define ARG_S (20)
 char *shl_strcat(char *dest, char *src);
 int shl_strcmp(char *str1, char *str2);
 int shl_strlen(char *str);
