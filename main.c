6298dec9f7b064131df962af0f37cc4cac9c567b
Mohakad <mohkadtrd@gmail.com>
2023-11-15 18:46:20 +0300

    task1

--- main.c
+++ main.c
@@ -1,31 +1,46 @@
-#include "simple.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 /**
- * main- shell
- * Return: 0
+ * main- no argumen
+ * Return: 0 or error
  */
 int main(void)
 {
-	char *userinp = NULL, **comm;
+	char usrinpt[100], *args[2];
 
-	size_t len = 0;
-	
-	ssize_t nread;
-
-	FILE *stream = stdin;
+	pid_t pid;
 
 	while (1)
 	{
-		printf(PROMPT);
-		nread = getline(&userinp, &len, stream);
-		if (nread == -1)
+		printf("($) ");
+		if (fgets(usrinpt, 100, stdin) == NULL)
+		{
+			printf("\n");
 			break;
-		userinp[nread - 1] = '\0';
-		if (strcmp(userinp, "") == 0)
-			continue;
-		comm = tocmd(userinp);
-		simp_exec(comm[0], comm);
-		free(comm);
+		}
+		/*usrinpt[strcspn(usrinpt, "\n")] = 0;*/
+		args[0] = usrinpt;
+		args[1] = NULL;
+		pid = fork();
+		if (pid == 0)
+		{
+			execve(usrinpt, args, NULL);
+			perror("execve");
+			exit(1);
+		}
+		else if (pid < 0)
+		{
+			perror("fork");
+			exit(1);
+		}
+		else
+		{
+			wait(NULL);
+		}
 	}
-	free(userinp);
 	return (0);
 }
