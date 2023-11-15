4391294666953ba0126ce70812fbc90ebbe75ac9
Mohakad <mohkadtrd@gmail.com>
2023-11-15 19:28:41 +0300

    main.h

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "main.h"
 /**
  * main- no argumen
  * Return: 0 or error
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAINH_H
+#define MAINH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#endif
