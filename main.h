3de97755af447d6f0ee9168db8d3fe399610d132
Mohakad <mohkadtrd@gmail.com>
2023-11-16 20:57:30 +0300

    fk

--- main.h
+++ main.h
@@ -13,9 +13,9 @@
 extern char **environ;
 #define COMM_LENGTH (1024)
 #define ARG_S (20)
-char *readinp(void);
-char **comspl(char *str);
-int executer(char **args);
+/*char *readinp(void);*/
+/*char **comspl(char *str);*/
+/*int executer(char **args);*/
 char *shl_strcpy(char *str2, char *str1); 
 void executer(char *userinpt);
 void tokniz(char *userinpt, char **args);
