a44d8463123059596c243362a2ccbef560ea4faf
Mohakad <mohkadtrd@gmail.com>
2023-11-24 11:23:25 -0800

    nv

--- main.c
+++ main.c
@@ -7,21 +7,18 @@
  */
 int main(int argc, char *argv[])
 {
+	const char *delim = " \n";
+    int notk = 0, i, status;
+    char *usrinpcp = NULL, *token;
 	pid_t pid;
 
-	char *buffer;
+	char *buffer = NULL;
 
-	size_t bufsize = 100;
+	size_t bufsize = 1024;
 
 	ssize_t ch;
 
 	(void)argc;
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror(argv[0]);
-		exit(1);
-	}
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
@@ -31,28 +28,55 @@ int main(int argc, char *argv[])
 		{
 			if (isatty(STDIN_FILENO))
 				write(STDOUT_FILENO, "\n", 1);
-			break;
+			return (-1);
 		}
-		buffer[ch - 1] = '\0';
+		usrinpcp = malloc(sizeof(char) * ch);
+          if (usrinpcp == NULL)
+          {
+                  perror(argv[0]);
+                  return (-1);
+	  }
+	  strcpy(usrinpcp, buffer);
+
+
+        token = strtok(usrinpcp, delim);
+
+        while (token != NULL){
+            notk++;
+            token = strtok(NULL, delim);
+        }
+        notk++;
+
+
+        argv = malloc(sizeof(char *) * notk);
+
+
+        token = strtok(buffer, delim);
+
+        for (i = 0; token != NULL; i++){
+            argv[i] = malloc(sizeof(char) * strlen(token));
+            strcpy(argv[i], token);
+
+            token = strtok(NULL, delim);
+        }
+        argv[i] = NULL;
 		pid = fork();
-		if (pid == -1)
-		{
-			perror(argv[0]);
-			free(buffer);
-			exit(1);
-		}
-		else if (pid == 0)
-		{
-			if (execve(buffer, argv, NULL) == -1)
-			{
-				perror(argv[0]);
-				free(buffer);
-				exit(1);
-			}
-		}
-		else
-			wait(NULL);
+		if (pid == 0) {
+
+   if (execve(argv[0], argv, NULL) == -1) {
+        perror(argv[0]);
+        exit(1);
+      }
+    } 
+	else if (pid > 0) 
+      wait(&status);
+	 else {
+      perror("fork");
+      exit(1);
+    }
 	}
 	free(buffer);
+	free(usrinpcp);
+	free(argv);
 	return (0);
 }
